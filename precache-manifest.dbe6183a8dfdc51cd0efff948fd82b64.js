6ac753101eb63c83249f19a3a784c7bfa0d2d387
Lucas Salvador <lucas.salvador11358@gmail.com>
2020-02-16 11:28:03 -0300

    adicionado build de aplicação

---
+++ precache-manifest.dbe6183a8dfdc51cd0efff948fd82b64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f04e459c8cf5efe7782446bfc052655",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e44db64bb399662ca834",
+    "url": "/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "95597e03f70280122ff6",
+    "url": "/static/js/2.f76336e7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.f76336e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e44db64bb399662ca834",
+    "url": "/static/js/main.1212886f.chunk.js"
+  },
+  {
+    "revision": "20b1cdc52743265d7634",
+    "url": "/static/js/runtime-main.d938c4f6.js"
+  }
+]);
